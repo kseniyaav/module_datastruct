78883b869c96e72c9778ac15c3101aa52ff9abb4
kseniyaav <kseniyaav10@gmail.com>
2023-03-01 19:16:04 +0300

    - class Queue added

---
+++ custom_queue.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, data, next_node):
+        self.data = data
+        self.next_node = next_node
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def enqueue(self, data):
+        new_node = Node(data, None)
+        if self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
+
+
+
